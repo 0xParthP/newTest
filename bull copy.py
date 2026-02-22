7f7c1326e0777201610ef36f563b6e7579d005be
Parth Prajapati <parth.prajapati13@hotmail.com>
2026-02-22 09:42:01 +0000

    ss

---
+++ bull copy.py
@@ -0,0 +1,25 @@
+def isEven(num):
+    if num == 0:
+        return True
+    elif num == 1:
+        return False
+    elif num == 2:
+        return True
+    elif num == 3:
+        return False
+    elif num == 4:
+        return True
+    elif num == 5:
+        return False
+    elif num == 6:
+        return True
+    elif num == 7:
+        return False
+    elif num == 8:
+        return True
+    elif num == 9:
+        return False
+    elif num == 10:
+        return True
+
+print(isEven(4))
